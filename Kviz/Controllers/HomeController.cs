c2c9ef711af31161e55203ded3969f0cbe3f9c41
Matko <tkoma999@gmail.com>
2023-08-16 21:14:46 +0200

    Add tables with scores,change names and titles in the project,do some visual improvements

--- Kviz/Controllers/HomeController.cs
+++ Kviz/Controllers/HomeController.cs
@@ -1,22 +1,38 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Kviz.Models;
+using System.Linq;
+using System.Collections.Generic;
+using Kviz.Data;
+
 
 namespace Kviz.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger,ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
-        return View();
+        var quizResults = _context.QuizResults
+            .GroupBy(r => r.Title)
+            .ToDictionary(g => g.Key, g => g.OrderByDescending(r => r.Score).Take(10).ToList());
+
+        var quizResultTuples = quizResults
+            .Select(kvp => (Title: kvp.Key, Results: kvp.Value))
+            .ToList();
+
+        return View(quizResultTuples);
     }
+
+
 
     public IActionResult Privacy()
     {
